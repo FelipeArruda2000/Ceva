d842af0f563c3fa548236d1c085d0a06fee198f3
Felipe Souza <felipe.souza6248@gmail.com>
2023-08-03 15:06:08 -0300

    atualização03/08

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 
 const quiz =[
     {	
-        q:'Em quais momentos podemos usar o adptil difusor ?',
-        options:['Em caso de agressividade entre os cães.','Na mudança de ambiente, introdução, adaptação a chegada de um novo filhote em casa, medos de barulhos (fogos).','Pode ser usado como sedativo no momento de estresse.'],
+        q:'Quando podemos indicar o Feliway Classic (Difusor e o Spray) ? ',
+        options:['O difusor è indicado em casos de conflitos e brigas, e o spray è indicado para espirar na face do felino como punição de má conduta. ','O difusor è indicado para promover conforto e reduzir o estresse do animal e o Spray è indicado para promover bem estar a ida ao veterinário, locais externos, demarcação por arranhaduras e por urina.','O spray è indicado para sedar o animal.'],
         answer:1
     },
     {
-        q:'Quando podemos indicar o feliway friends?',
-        options:['sua composição provoca incômodo, então devemos usar como forma de punição.','No auxilio para diminuição das brigas e sinais de conflitos entre gatos.','È necessario usar quando o animal tem boa convivencia com o outro integrante da casa.'],
+        q:'Em quais momentos podemos indicar o Feliway Friends?',
+        options:['Pode ser indicado em casos de boa convivência entre gatos. ','Pode ser indicado em situação de brigas e sinais de conflito entre gatos.','Pode ser indicado como punição para fazer com que os gatos parem de brigar.'],
         answer:1
     },
     {	
-        q:'Quando indicamos o feliway classic difusor e spray ?',
-        options:['No momento de brigas e conflitos intensos entre os gatos.','O spray è indicado para espirrar no rosto do animal.','Na mudança de ambiente, introdução de novos integrantes e medo de barulho.'],
+        q:'Em quais momentos podemos usar o adaptil difusor ?',
+        options:['Em momentos de estresse que você utiliza para sedar o animal.','Em casos de agressividade entre outros cães. ','Em momentos como: Mudança de ambiente, Introdução, adaptação a chegada de um novo integrante na casa, medo de barulhos e fogos.'],
         answer:2
     },
     {	
-        q:'Quais vermifugos da nossa linha trata além dos vermes a giardia?',
-        options:['Apenas o canex premium.','Nenhuma das alternativas.','Canex premium e canex plus 3.'],
+        q:'Quais vermífugos da linha trata além dos vermes, a giardia?',
+        options:['Nenhuma das alternativas.','Apenas o Canex Premium.','Canex Premium e Canex Giárdia.'],
         answer:2
     },
     {	
-        q:'Qual vermifugo trata o verme do coração?',
-        options:['nenhuma das alternativas.','canex premium','Canex plus 3'],
+        q:'Qual o Período de ação contra pulgas do Vectra 3D para cães e o período de ação do Vectra Gatos? ',
+        options:['Ação 1 mês para cães e gatos.','Ação de 2 meses para cães e 60 dias para gatos (7 semanas) .','Ação de 1 mês para cães e dois meses para gatos.'],
         answer:1
     },
     {	
-        q:"Qual vermifugo è indicado para cadelas prenhas e trata o verme trichuris vulpis?",
-        options:['Linha Canex Plus.','Linha Canex premium ','Linha Petzi plus.'],
+        q:"Qual vermifugo è indicado para cadelas prenhas e lactantes e trata o verme (trichuris vulpis) ?",
+        options:['Linha Canex Plus.',' linha Canex plus. ','A linha Petzi plus.'],
         answer:2
     },
     {	
-        q:"O que o Vectra cães 3D previne?",
-        options:["Contra pulga, carrapato e repele mosquitos e flebotomos.","Apenas contra Pulga.","contra pulgas e carrapatos apenas."],
+        q:"O que o Vectra 3D para cães previne?",
+        options:["Previne contra pulgas e carrapatos apenas. ","Apenas contra pulgas.","Previne contra pulgas, carrapatos e repele contra mosquitos e flebótomos. "],
         answer:0
     },
     {
-        q:"Vectra 3D tem ação de quantos meses para pulgas em caes e gatos?",
-        options:["Ação de 2 meses para caes e gatos","Ação de 1 mês para caes e 2 meses para gatos.","Ação de 1 mês para caes e gatos."],
+        q:"Qual o vermífugo da linha previne contra o verme do coração (dirofilariose canina)? ",
+        options:["Canex premium","Canex plus 3.","Nenhuma das alternativas. "],
         answer:0
     },
     ]
